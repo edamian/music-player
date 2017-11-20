67dc73e6c7424ee7b46f4f09061c61dfc1a83588
Edgar Xolop <edgarxoix@gmail.com>
2017-11-19 20:05:06 -0600

    anko dependency & READ_EXTERNAL_STORAGE added

--- app/src/main/java/edamian/com/music_player/MainActivity.kt
+++ app/src/main/java/edamian/com/music_player/MainActivity.kt
@@ -1,19 +1,51 @@
 package edamian.com.music_player
 
+import android.Manifest
 import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.os.Environment
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.widget.Button
-import android.widget.Toast
+import android.widget.EditText
+
+import org.jetbrains.anko.alert
+
+import java.io.File
+
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btnTouchMe = findViewById<Button>(R.id.btnTouchMe)
 
-        btnTouchMe.setOnClickListener{
-            Toast.makeText(this.applicationContext,"Hey!",Toast.LENGTH_SHORT).show()
+        loadPermissions()
+
+        val btnPlay = findViewById<Button>(R.id.btnPlay)
+        val btnLoad = findViewById<Button>(R.id.btnLoad)
+        val txtDirectory = findViewById<EditText>(R.id.txtDirectory)
+
+        txtDirectory.setText(Environment.getExternalStorageDirectory().toString())
+
+        btnPlay.setOnClickListener{
+            alert ( "Playing music." ,"Hey!" ).show()
+        }
+
+        btnLoad.setOnClickListener{
+            val path = txtDirectory.text.toString()
+
+                val directory = File(path)
+                val files = directory.listFiles().size
+        }
+    }
+
+    fun loadPermissions(){
+        if(ContextCompat.checkSelfPermission(this.applicationContext,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            if(!ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),1)
+            }
         }
     }
 
